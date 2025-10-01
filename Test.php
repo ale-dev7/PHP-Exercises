30ec6fb31452875553d54d325fa83edd93962dc4
ale-def7 <olesea.cretu@mailfence.com>
2025-10-01 10:45:08 +0200

    Initial commit

---
+++ Test.php
@@ -0,0 +1,19 @@
+<?php
+
+//Hier kommen Aufgaben über das Thema Programmierung.
+
+
+function hello()
+{
+
+    echo "<pre>";
+    echo "Hello World!\n";
+    echo "Hello World My Name is Sam\n";
+    echo "Hello World I like Programming\n";
+    echo "Hello World This BBQ-School is awesome\n";
+
+    echo "''''''''''''''''''''''''''''''''''''''''";
+    echo "</pre>";
+}
+
+hello();
